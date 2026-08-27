dee8c0c2eb5576369514c84ae8c2ebc66133ac64
PRBode <Peer_Bod3@web.de>
2026-08-27 11:06:03 +0000

    modified:   .vscode/settings.json 	new file:   Aufgabe_1.py

---
+++ Aufgabe_1.py
@@ -0,0 +1,54 @@
+#!/user/bin/env python3
+
+# Variablen
+
+a = 10
+b = 5
+c = 3
+
+#addition
+summe = a + b + c
+print("Die Summe ergibt:", summe)
+
+#Subtraktion
+differenz = a - b
+print("Die Differenz beträgt:", differenz)
+
+#Multiplikation
+produkt = a * b
+print("Das Ergebnis der Multiplikation beträgt:", produkt)
+
+#Division
+quotient = a / b
+print("Der Quotient beträgt:", quotient)
+
+#Ganzzahlige Division
+ganzzahl_quotient = a // c
+print("Der Ganzzahlige Quotient beträgt:", ganzzahl_quotient)
+
+#Modulo (gibt den Rest einer Division)
+rest = a % c
+print("Der Rest der Division beträgt:", rest)
+
+#Potenzierung
+potenz = 2 **c
+print("2 hoch 3 ergibt:", potenz)
+
+# Aufgabe:
+# Führen Sie eine Addition, Subtraktion, Multiplikation und Division
+# mit zwei von Ihnen gewählten Variablen durch und geben Sie die Ergebnisse aus.
+
+print("---------------------------------------------")
+print("Aufgabe:")
+
+aufgabe_addition = a + c
+print("Addition:", aufgabe_addition)
+
+aufgabe_division = a - c
+print("Division:", aufgabe_division)
+
+aufgabe_multiplikation = a * c 
+print("Multiplikation:", aufgabe_multiplikation)
+
+aufgabe_division = a / b
+print("Division:", aufgabe_division)
